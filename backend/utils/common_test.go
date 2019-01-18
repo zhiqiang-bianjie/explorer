0d472ed20c574918dab162b6725f9def28e852f5
zhangzhiqiang <745124335@aa.com>
2019-01-18 13:44:50 +0800

    subscribe event of irishub

--- backend/utils/common_test.go
+++ backend/utils/common_test.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/websocket"
+	"log"
 	"testing"
 )
 
@@ -14,4 +18,79 @@ func TestParseInt(t *testing.T) {
 
 	_, ok = ParseInt("sd")
 	assert.False(t, ok)
+}
+
+const EventNewBlock = "tm.event='NewBlock'"
+const EventNewTx = "tm.event='Tx'"
+
+func TestTendermintWs(t *testing.T) {
+	var wsUrl = "ws://192.168.150.7:30657/websocket"
+	var origin = "http://192.168.150.7:30657/"
+	ws, err := websocket.Dial(wsUrl, "", origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var req = RpcReq{
+		Jsonrpc: "2.0",
+		Method:  "subscribe",
+		ID:      "0",
+		Params: Params{
+			Query: EventNewTx,
+		},
+	}
+
+	bz, _ := json.Marshal(req)
+
+	if _, err := ws.Write(bz); err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		var msg = make([]byte, 10*1024)
+		var n int
+		if n, err = ws.Read(msg); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", msg[:n])
+	}
+
+}
+
+type RpcReq struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Method  string `json:"method"`
+	ID      string `json:"id"`
+	Params  Params `json:"params"`
+}
+
+type Params struct {
+	Query string `json:"query"`
+}
+
+type RpcResp struct {
+	Jsonrpc string `json:"jsonrpc"`
+	ID      string `json:"id"`
+	Result  struct {
+		Query string `json:"query"`
+		Data  struct {
+			Type  string `json:"type"`
+			Value struct {
+				TxResult struct {
+					Height string `json:"height"`
+					Index  int    `json:"index"`
+					Tx     string `json:"tx"`
+					Result struct {
+						Log       string `json:"log"`
+						GasWanted string `json:"gas_wanted"`
+						GasUsed   string `json:"gas_used"`
+						Tags      []struct {
+							Key   string `json:"key"`
+							Value string `json:"value"`
+						} `json:"tags"`
+					} `json:"result"`
+				} `json:"TxResult"`
+			} `json:"value"`
+		} `json:"data"`
+	} `json:"result"`
 }
